6623977172cef42ad01c5ba5de945ca4dfa41d47
matangranottau <matangranot@mail.tau.ac.il>
2024-12-05 16:02:27 +0200

    Working version

--- ex2/ex2.c
+++ ex2/ex2.c
@@ -5,9 +5,10 @@
 // SECTION A: Recursive Subsequences Generation
 void generate_subsequences(char* str, char* current, int index) {
     // TODO: Implement the recursive logic for generating subsequences
-    if (strlen(str) == index + 1) { // If only one char: 
-        if (strlen(current) > 0) {
-            printf("%s\n", current);//
+    int current_len = strlen(current);
+    if (strlen(str) == index + 1) {
+        if (current_len > 0) {
+            printf("%s\n", current);
             printf("%s%c\n", current, str[index]);
         }
         else {
@@ -16,9 +17,9 @@ void generate_subsequences(char* str, char* current, int index) {
         
     }   
     else {
-        generate_subsequences(str, current, index + 1); // Print without the char
-        current[strlen(current)] = str[index];
-        current[strlen(current) + 1] = '\0';
+        generate_subsequences(str, current, index + 1);
+        current[current_len] = str[index];
+        current[current_len + 1] = '\0';
         generate_subsequences(str, current, index + 1);
 
     }
