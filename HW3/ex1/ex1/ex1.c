c2dcd5464625503c1363a2f2082a0cc6a4ecf0d8
matangranottau <matangranot@mail.tau.ac.il>
2024-12-14 00:21:33 +0200

    First Shell

---
+++ HW3/ex1/ex1/ex1.c
@@ -0,0 +1,56 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#define MAX_SIZE 10
+
+void create_board(char board[][2 * MAX_SIZE], int N) {
+	/* Assign to board: Nx2N nested array. */
+}
+void print_board(const char board[][2 * MAX_SIZE]) {
+	/* Prints board. */
+}
+void update_board(char board[][2 * MAX_SIZE], const char* current_mark) {
+	/*
+	1. Ask for Indices, and check if valid.
+	2. Sepreates to row and column.
+	3. Updates board in the location, with the current mark.
+	*/
+}
+int check_won(const char board[][2 * MAX_SIZE], const char* current_mark) {
+	/* return 1 if full row/coloumn/diagnol with mark*/
+	return 0;
+}
+void pass_turn(char* current_player) {
+	/* Alternate current player and mark*/
+}
+
+
+int main() {
+	char board[MAX_SIZE][2 * MAX_SIZE];
+	const char player_1 = 1, mark_1 = 'X', player_2 = 2, mark_2 = 'O';
+	const char* current_player = &player_1;
+	const char* current_mark = &mark_1;  // const char* - pointer to a constant char (the pointer itself can point on another char const)
+	int N, won = 0, turn_num = 0;
+	printf("Please enter the board size N [1-10]:\n");
+	scanf("%d", &N);
+	create_board(board, N);
+	printf("Welcome to %dx%d Tic-Tac-Toe:\n", N, N);
+	print_board(board);
+	do
+	{
+		turn_num++;
+		printf("Player %d, please insert your move :\n", *current_player);
+		update_board(board, current_mark);
+		print_board(board);
+		won = check_won(board, current_mark);
+		if (!won) {
+			pass_turn(current_player);
+		}
+	} while (!won && turn_num < N * N); // max iterations turn num = N^2
+	if (won) {
+		printf("Player _ is the winner!\n", *current_player);
+	}
+	printf("There is a Tie!\n");
+	return 0;
+}
