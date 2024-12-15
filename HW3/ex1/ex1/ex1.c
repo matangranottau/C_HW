7f48ef83118b6cab39eab594796142587bcba500
matangranottau <matangranot@mail.tau.ac.il>
2024-12-15 17:23:34 +0200

    Revert "get_idx() still gay"
    This reverts commit 9eff75c7dd9c7ae28e8f2d76ea7524803036181c.

--- HW3/ex1/ex1/ex1.c
+++ HW3/ex1/ex1/ex1.c
@@ -81,8 +81,8 @@ void pass_turn(const char* current_player) {
 }
 
 //test main
-
-int main() {
+/*
+  int main() {
 
 	int* idx;
 	idx = get_idx();
@@ -90,8 +90,8 @@ int main() {
 	return 0;
 		
 }
+*/
 
-/*
 int main() {
 	char board[MAX_SIZE][2 * MAX_SIZE];
 	const char player_1 = 1, mark_1 = 'X', player_2 = 2, mark_2 = 'O';
@@ -120,4 +120,3 @@ int main() {
 	printf("There is a Tie!\n");
 	return 0;
 }
-*/
