1f9279f05d8993e516c41bcd151ab96bf806aa22
matangranottau <matangranot@mail.tau.ac.il>
2024-12-19 12:15:56 +0200

    fixed: no trailing spaces in print_board

--- HW3/ex1/ex1/ex1.c
+++ HW3/ex1/ex1/ex1.c
@@ -16,7 +16,12 @@ void print_board(char board[][MAX_SIZE], int N) {
     // Print the current board state with spaces
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
-            printf("%c ", board[i][j]);
+            if (j != N - 1) {
+                printf("%c ", board[i][j]);
+            }
+            else {
+                printf("%c", board[i][j]);
+            }
         }
         printf("\n");
     }
