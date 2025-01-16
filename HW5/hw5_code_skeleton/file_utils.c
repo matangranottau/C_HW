9db07dff93ffd45073cf2bbdd569e4abfa5c866c
Yogev Grinblatt <yogevg@mail.tau.ac.il>
2025-01-16 17:48:57 +0200

    starting implement file_utils

--- HW5/hw5_code_skeleton/file_utils.c
+++ HW5/hw5_code_skeleton/file_utils.c
@@ -1,16 +1,42 @@
 #include "file_utils.h"
 #include "error_types.h"
+#include <stdio.h>
 
 #define MAX_WORD_SIZE 256
 
 int allocate_buffer(void **buf, unsigned int buf_size) {
   // TODO
+    if (buf == NULL ){
+        return ERR_NULL_PTR;
+    }
+    if (buf_size == 0) {
+        return ERR_BAD_FUNC_ARG;
+    }
+    *buf = malloc(buf_size);
+    if (*buf = NULL) {
+        return ERR_MEMORY;
+    }
   return OK;
 }
 
 int write_data_to_file(const char *output_file_path, const unsigned char *buf,
                        unsigned int buf_size) {
   // TODO
+    if (output_file_path == NULL || buf == NULL) {
+        return ERR_NULL_PTR;
+    }
+    if (buf_size == 0) {
+        return ERR_BAD_ENC_TYPE;
+    }
+    FILE* file = fopen(output_file_path, "wb"); // open file
+    if (file == NULL) {
+        return ERR_FILE;
+    }
+    size_t written = fwrite(buf, 1, buf_size, file); // write to the file
+    fclose(file); // close the fike
+    if (written != buf_size) {
+        return ERR_FILE;
+    }
   return OK;
 }
 
