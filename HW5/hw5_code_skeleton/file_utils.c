5579883ed85572b3c0102ce9779ceb41fa79c4d1
matangranottau <matangranot@mail.tau.ac.il>
2025-01-17 11:39:56 +0200

    write_data_to_file

--- HW5/hw5_code_skeleton/file_utils.c
+++ HW5/hw5_code_skeleton/file_utils.c
@@ -1,6 +1,7 @@
 #include "file_utils.h"
 #include "error_types.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 #define MAX_WORD_SIZE 256
 
@@ -19,13 +20,25 @@ int allocate_buffer(void **buf, unsigned int buf_size) {
 
 int write_data_to_file(const char *output_file_path, const unsigned char *buf,
                        unsigned int buf_size) {
-  // TODO
-  return OK;
+    FILE* fPtrWrite = NULL;
+    fPtrWrite = fopen(output_file_path, "w");
+    
+    if (buf == NULL) {
+        return ERR_NULL_PTR;
+    }
+    if (fPtrWrite == NULL) {
+        return ERR_FILE;
+    }
+
+    fputs(buf, fPtrWrite);
+
+    free(buf);
+
+    return OK;
 }
 
 int load_data_from_file(const char *input_file_path, unsigned char **buf,
                         unsigned int *buf_size) {
-  // TODO
   return OK;
 }
 
