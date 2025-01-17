fa23c2ef81d4798339fb32183b5e5acc4c4aaa2d
Yogev Grinblatt <yogevg@mail.tau.ac.il>
2025-01-17 12:06:45 +0200

    file_utiles suppoused to work

--- HW5/hw5_code_skeleton/file_utils.c
+++ HW5/hw5_code_skeleton/file_utils.c
@@ -1,3 +1,4 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include "file_utils.h"
 #include "error_types.h"
 #include <stdio.h>
@@ -28,7 +29,7 @@ int write_data_to_file(const char *output_file_path, const unsigned char *buf,
     if (buf_size == 0) {
         return ERR_BAD_ENC_TYPE;
     }
-    FILE* file = fopen(output_file_path, "wb"); // open file
+    FILE* file = fopen(output_file_path, "w"); // open file
     if (file == NULL) {
         return ERR_FILE;
     }
@@ -43,6 +44,33 @@ int write_data_to_file(const char *output_file_path, const unsigned char *buf,
 int load_data_from_file(const char *input_file_path, unsigned char **buf,
                         unsigned int *buf_size) {
   // TODO
+    if (input_file_path == NULL || buf == NULL || buf_size == NULL) {
+        return ERR_NULL_PTR;
+    }
+    FILE* file = fopen(input_file_path, "r"); // open file
+    if (file == NULL) {
+        return ERR_FILE;
+    }
+
+    fseek(file, 0, SEEK_END);
+    long size = ftell(file); //checking the size of the file
+    rewind(file);
+    if (size < 0) {
+        fclose(file);
+        return ERR_FILE;
+    }
+    *buf = malloc(buf_size);
+    if (*buf = NULL) {
+        return ERR_MEMORY;
+    }
+    size_t read = fread(*buf, 1, size, file); // reading
+    fclose(file);
+
+    if (read != size) { // error in reading thr file
+        free(*buf);
+        return ERR_FILE;
+    }
+    *buf_size = (unsigned int)size;
   return OK;
 }
 
@@ -50,5 +78,42 @@ int load_data_from_file(const char *input_file_path, unsigned char **buf,
 int load_array_of_words(const char *input_file_path, char ***array,
                         unsigned int *p_cnt) {
   // TODO
+    if (input_file_path == NULL || array == NULL || p_cnt == NULL) {
+        return ERR_NULL_PTR;
+    }
+    FILE* file = fopen(input_file_path, "r");
+    if (file == NULL) {
+        return ERR_FILE;
+    }
+    char** words = NULL; 
+    char word[MAX_WORD_SIZE];
+    unsigned int cnt = 0;
+
+    while (fscanf(file, "% 255s", word) == 1) {
+        char** temp = realloc(words, (cnt + 1) * sizeof(char*));
+        if (temp == NULL) {
+            fclose(file);
+            for (int i = 0; i < cnt; i++) {
+                free(words[i]);
+            }
+            free(words);
+            return ERR_MEMORY;
+        }
+        words = temp;
+        words[cnt] = malloc(strlen(word) + 1);
+        if (words[cnt] == NULL) {
+            fclose(file);
+            for (int i = 0; i < cnt; i++) {
+                free(words[i]);
+            }
+            free(words);
+            return ERR_MEMORY;
+        }
+        strcpy(words[cnt], word);
+        cnt++;
+    }
+    fclose(file);
+    *array = words;
+    p_cnt = cnt;
   return OK;
 }
