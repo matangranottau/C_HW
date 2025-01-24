3ae5ee2bfcbdb39fd6c05e6da7aad96fde206c01
matan <matan@EL-ROEI>
2025-01-24 15:24:52 +0200

    test files

--- HW5/hw5_code_skeleton/main.c
+++ HW5/hw5_code_skeleton/main.c
@@ -6,7 +6,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "algs.h"
-#define MAIN_ON 1
+#define MAIN_ON 0
 #define TEST_ADD_SUFFIX 0
 #define TEST_CENSOR 0
 
@@ -52,48 +52,13 @@ int main(int argc, const char* argv[]) {
 
 	return 0;
 }
+#else
+int main(char* argv[], int argc) {
+	test_main();
+	return 0;
+}
+
 #endif // MAIN_ON
 
 
-#if TEST_CENSOR
-int main(int argc, const char* argv[]) {
-	char* buff_1 = "Hello, im matan";
-	char* cen_1[2] = { "Hello", "im" };
-	//censor(&buff_1, cen_1, 15, 2);
-	//printf("%s\n", buff_1);
 
-	char buff_2[12] = {'h', 'e', 'l', 'l', 'o', 'i', 'm', 'm', 'a', 't', 'a', 'n'};
-	char* cen_2[2] = { "Hello", "im" };
-	//censor(&buff_2, cen_2, 12, 2);
-	//printf("%s\n", buff_2);
-	
-
-	char* hds_str = "can you save?\n, can you save my..?\n can you save my .. HEAVYDIRTYSOUL??";
-	printf("strlen of str1 + 1 is %d\n", strlen(hds_str) + 1);
-	char* str_2 = (char*)malloc(strlen(hds_str) + 1);
-	if (str_2 == NULL) {
-		printf("error\n");
-		return 1;
-	}
-	strcpy(str_2, hds_str);
-	censor_string(&str_2, "save", strlen(str_2), strlen("save"));
-	printf("%s\n", str_2);
-	return 0;
-}
-#endif // TEST_CENSOR
-
-
-#if TEST_ADD_SUFFIX
-int main(int argc, const char* argv[]) {
-	char* out;
-	char* in_1 = "text.txt";
-	char* in_2 = "blahblahblahh\out.txt";
-	add_suffix(in_1, &out);
-	printf("%s\n", out);
-
-	add_suffix(in_2, &out);
-	printf("%s\n", out);
-
-	return 0;
-}
-#endif // TEST_ADD_SUFFIX
---
+++ HW5/hw5_code_skeleton/test.c
@@ -0,0 +1,59 @@
+#include "algs.h"
+#ifndef MAIN_ON
+
+
+
+
+// int Flip_Even(const unsigned char* data_in, unsigned int size_in, unsigned char* data_out, unsigned int size_out);
+
+int test_flip_even(char test_case, char expected) {
+	char buf[1] = { test_case };
+	char output[1];
+	buf[0] = (char)test_case;
+	Flip_Even(buf, 1, output, 1);
+	if (output[0] != expected) {
+		printf("test_flip_even: Error! input %d, expected %d, got %d\n", test_case, expected, buf[0]);
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+
+int test_swap_3(char test_case, char expected) {
+	char buf[1] = { test_case };
+	char output[1];
+	buf[0] = (char)test_case;
+	Flip_Even(buf, 1, output, 1);
+	if (output[0] != expected) {
+		printf("test_flip_even: Error! input %d, expected %d, got %d\n", test_case, expected, buf[0]);
+	}
+	else {
+		printf("test_flip_even: case %d passed\n", test_case);
+	}
+}
+
+int test_rot5(char test_case, char expected) {
+	char buf[1] = { test_case };
+	char output[1];
+	buf[0] = (char)test_case;
+	Flip_Even(buf, 1, output, 1);
+	if (output[0] != expected) {
+		printf("test_flip_even: Error! input %d, expected %d, got %d\n", test_case, expected, buf[0]);
+	}
+	else {
+		printf("test_flip_even: case %d passed\n", test_case);
+	}
+}
+
+int test_main() {
+	int tests_failed = 0;
+	tests_failed += test_flip_even(0b11111111, 0b01010101);
+	tests_failed += test_flip_even(0b10110011, 0b00011001);
+	
+
+	printf("%d tests failed\n", tests_failed);
+}
+
+
+#endif // !MAIN_ON
